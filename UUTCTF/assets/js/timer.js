4256d19f3b52af7879cfc853ff58b33d77c28568
yousef <gheibiyousef2000@gmail.com>
2022-05-24 21:15:59 +0430

    update ctf website to 2022

--- UUTCTF/assets/js/timer.js
+++ UUTCTF/assets/js/timer.js
@@ -1,19 +1,19 @@
-(function () {
+(function() {
     const second = 1000,
-          minute = second * 60,
-          hour = minute * 60,
-          day = hour * 24;
-  
-    let competition = "Oct 31, 2021 13:00:00",
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    let competition = "Jun 1, 2022 13:00:00",
         countDown = new Date(competition).getTime(),
-        x = setInterval(function() {    
-  
-          let now = new Date().getTime(),
-              distance = countDown - now;
-  
-          document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+        x = setInterval(function() {
+
+            let now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
         }, 0)
-    }());
+}());
